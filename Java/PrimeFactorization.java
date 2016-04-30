4b12ee374e2f40afabf5396cf707d3a29418a33e
Bethy Diakabana <bethyd123@gmail.com>
2016-04-30 14:30:44 -0400

    Update PrimeFactorization.java

--- Java/PrimeFactorization.java
+++ Java/PrimeFactorization.java
@@ -2,8 +2,7 @@ package net.bethydiakabana;
 
 public class PrimeFactorization {
 	public static void main(String[] args) {
-//		long N = Long.parseLong(args[0]);
-		long N = 23456;
+		long N = Long.parseLong(args[0]);
 		// uses exponents for efficiency
 		for (int i = 2; i*i <= N; i++) {
 			// continues to divide through if i is a factor of N
